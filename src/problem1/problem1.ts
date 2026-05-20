44102f21ff27f56b411e404d15f2e9fcb4e370f6
neverluckyD <thanhduyhuynh2910@gmail.com>
2026-05-20 20:48:27 +0700

    add problem 1 answer

---
+++ src/problem1/problem1.ts
@@ -0,0 +1,23 @@
+// Time complexity: O(n)
+// Space complexity: O(1)   
+var sum_to_n_a = function (n: number): number {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+};
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+var sum_to_n_b = function (n: number): number {
+    return n * (n + 1) / 2;
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+var sum_to_n_c = function (n: number): number {
+    if (n === 1) return 1;
+
+    return n + sum_to_n_c(n - 1);
+};
